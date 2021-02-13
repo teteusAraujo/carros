d165937e2b9f3698f6fa7ff5cf8c6566b3224905
Mateus Araújo <mateusaraujo996@gmail.com>
2021-02-13 17:19:24 -0300

    Alteração na classe CarrosController

--- src/main/java/com/mateus/carros/controller/CarrosController.java
+++ src/main/java/com/mateus/carros/controller/CarrosController.java
@@ -3,6 +3,7 @@ package com.mateus.carros.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -26,8 +27,14 @@ public class CarrosController {
 		return service.getCarros();
 	}
 	@GetMapping("/{id}")
-	public Optional<Carro> listarCarro(@PathVariable("id") Long id){
-		return service.getCarro(id);
+	public ResponseEntity listarCarro(@PathVariable("id") Long id){
+		Optional<Carro> carro = service.getCarro(id);
+		
+		if(carro.isPresent()) {
+			return ResponseEntity.ok(carro.get());
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 	
 	@GetMapping("/tipo/{tipo}")
